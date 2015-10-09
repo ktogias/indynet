1651c8dcd2d4cb579f64fb44a7874e332c5a4281
Konstantinos Togias <info@ktogias.gr>
2015-10-10 02:28:23 +0300

    Passing custom fields for each event class

--- src/plugins/Indynet/FetchCallback.java
+++ src/plugins/Indynet/FetchCallback.java
@@ -13,6 +13,12 @@ import freenet.client.async.ClientGetCallback;
 import freenet.client.async.ClientGetter;
 import freenet.client.events.ClientEvent;
 import freenet.client.events.ClientEventListener;
+import freenet.client.events.ExpectedHashesEvent;
+import freenet.client.events.ExpectedMIMEEvent;
+import freenet.client.events.FinishedCompressionEvent;
+import freenet.client.events.SplitfileCompatibilityModeEvent;
+import freenet.client.events.SplitfileProgressEvent;
+import freenet.client.events.StartedCompressionEvent;
 import freenet.clients.fcp.FCPPluginConnection;
 import freenet.clients.fcp.FCPPluginMessage;
 import freenet.keys.FreenetURI;
@@ -21,7 +27,9 @@ import freenet.node.RequestClient;
 import freenet.support.SimpleFieldSet;
 import freenet.support.io.ResumeFailedException;
 import java.io.IOException;
+import java.sql.Timestamp;
 import java.util.Arrays;
+import java.util.TimeZone;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -173,11 +181,57 @@ public class FetchCallback implements ClientGetCallback, RequestClient, ClientEv
                 params.putSingle("eventclass", ce.getClass().getName());
                 params.put("eventcode", ce.getCode());
                 params.putSingle("eventdescription", ce.getDescription());
+                if(ce instanceof SplitfileProgressEvent) {
+                    eventToParams((SplitfileProgressEvent) ce, params);
+                }
+                else if (ce instanceof ExpectedMIMEEvent){
+                    eventToParams((ExpectedMIMEEvent)ce, params);
+                }
+                else if (ce instanceof SplitfileCompatibilityModeEvent){
+                    eventToParams((SplitfileCompatibilityModeEvent)ce, params);
+                }
+                else if (ce instanceof ExpectedHashesEvent){
+                    eventToParams((ExpectedHashesEvent)ce, params);
+                }
                 pluginConnection.send(FCPPluginMessage.constructReplyMessage(pluginMessage, params, null, false, "", ""));
             } catch (IOException ex) {
                 Logger.getLogger(FetchCallback.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
+    }
+    
+    
+    private void eventToParams(SplitfileProgressEvent ce, SimpleFieldSet params) {
+        params.put("finalizedTotal", ce.finalizedTotal);
+        params.put("totalBlocks", ce.totalBlocks);
+        params.put("succeedBlocks", ce.succeedBlocks);
+        params.put("failedBlocks", ce.failedBlocks);
+        params.put("fatallyFailedBlocks", ce.fatallyFailedBlocks);
+        params.put("minSuccessFetchBlocks", ce.minSuccessFetchBlocks);
+        params.put("minSuccessfulBlocks", ce.minSuccessfulBlocks);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        if (ce.latestSuccess != null){
+            Timestamp latestSuccess = new Timestamp(ce.latestSuccess.getTime());
+            params.putSingle("latestSuccess", latestSuccess.toLocalDateTime().toString());
+        }
+        if (ce.latestFailure != null){
+            Timestamp latestFailure = new Timestamp(ce.latestFailure.getTime());
+            params.putSingle("latestFailure", latestFailure.toLocalDateTime().toString());
+        }
+
+    }
+    
+    private void eventToParams(ExpectedMIMEEvent ce, SimpleFieldSet params){
+        params.putSingle("expectedMIMEType", ce.expectedMIMEType);
+    }
+
+    private void eventToParams(SplitfileCompatibilityModeEvent ce, SimpleFieldSet params){
+        params.putSingle("maxCompatibilityMode", ce.maxCompatibilityMode.name());
+        params.putSingle("minCompatibilityMode", ce.minCompatibilityMode.name());
+    }
+    
+    private void eventToParams(ExpectedHashesEvent ce, SimpleFieldSet params){
+        params.put("hashesLength", ce.hashes.length);
     }
     
     public int getStatus() throws InterruptedException {
--- src/plugins/Indynet/InsertCallback.java
+++ src/plugins/Indynet/InsertCallback.java
@@ -13,6 +13,10 @@ import freenet.client.async.ClientPutCallback;
 import freenet.client.async.ClientPutter;
 import freenet.client.events.ClientEvent;
 import freenet.client.events.ClientEventListener;
+import freenet.client.events.ExpectedHashesEvent;
+import freenet.client.events.FinishedCompressionEvent;
+import freenet.client.events.SplitfileProgressEvent;
+import freenet.client.events.StartedCompressionEvent;
 import freenet.clients.fcp.FCPPluginConnection;
 import freenet.clients.fcp.FCPPluginMessage;
 import freenet.keys.FreenetURI;
@@ -23,6 +27,8 @@ import freenet.support.api.Bucket;
 import freenet.support.api.RandomAccessBucket;
 import freenet.support.io.ResumeFailedException;
 import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.TimeZone;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -277,11 +283,57 @@ public class InsertCallback implements ClientPutCallback, RequestClient, ClientE
                 params.putSingle("eventclass", ce.getClass().getName());
                 params.put("eventcode", ce.getCode());
                 params.putSingle("eventdescription", ce.getDescription());
+                if(ce instanceof SplitfileProgressEvent) {
+                    eventToParams((SplitfileProgressEvent) ce, params);
+                }
+                else if (ce instanceof StartedCompressionEvent){
+                    eventToParams((StartedCompressionEvent)ce, params);
+                }
+                else if (ce instanceof FinishedCompressionEvent){
+                    eventToParams((FinishedCompressionEvent)ce, params);
+                }
+                else if (ce instanceof ExpectedHashesEvent){
+                    eventToParams((ExpectedHashesEvent)ce, params);
+                }
                 pluginConnection.send(FCPPluginMessage.constructReplyMessage(pluginMessage, params, null, false, "", ""));
             } catch (IOException ex) {
                 Logger.getLogger(FetchCallback.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
+    
+    
+    private void eventToParams(SplitfileProgressEvent ce, SimpleFieldSet params) {
+        params.put("finalizedTotal", ce.finalizedTotal);
+        params.put("totalBlocks", ce.totalBlocks);
+        params.put("succeedBlocks", ce.succeedBlocks);
+        params.put("failedBlocks", ce.failedBlocks);
+        params.put("fatallyFailedBlocks", ce.fatallyFailedBlocks);
+        params.put("minSuccessFetchBlocks", ce.minSuccessFetchBlocks);
+        params.put("minSuccessfulBlocks", ce.minSuccessfulBlocks);
+        TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        if (ce.latestSuccess != null){
+            Timestamp latestSuccess = new Timestamp(ce.latestSuccess.getTime());
+            params.putSingle("latestSuccess", latestSuccess.toLocalDateTime().toString());
+        }
+        if (ce.latestFailure != null){
+            Timestamp latestFailure = new Timestamp(ce.latestFailure.getTime());
+            params.putSingle("latestFailure", latestFailure.toLocalDateTime().toString());
+        }
 
+    }
+    
+    private void eventToParams(StartedCompressionEvent ce, SimpleFieldSet params){
+        params.putSingle("codec", ce.codec.name);
+    }
+
+    private void eventToParams(FinishedCompressionEvent ce, SimpleFieldSet params){
+        params.put("codec", ce.codec);
+        params.put("originalSize", ce.originalSize);
+        params.put("compressedSize", ce.compressedSize);
+    }
+    
+    private void eventToParams(ExpectedHashesEvent ce, SimpleFieldSet params){
+        params.put("hashesLength", ce.hashes.length);
+    }
 }
